d1da83038f0ae29e95ea5dd40562520670772189
namandoshi8 <workspace.naman@gmail.com>
2024-12-26 00:37:55 +0530

    added mui support and header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import { Button, IconButton } from "@mui/material";
+import logoImg from "../assets/logo.jpg";
+
+function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="Logo" />
+        <h1>ReactMeals</h1>
+      </div>
+      <div>
+        <nav>
+          {/* <button>Cart {0}</button> */}
+          <Button size="small" variant="contained">
+            Cart
+          </Button>
+          {/* <IconButton color="primary" aria-label="add to shopping cart">
+            Cart
+          </IconButton> */}
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
