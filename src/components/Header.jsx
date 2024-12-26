1963b57a3a7205bf763f177a7c79f0d5bf8172d3
namandoshi8 <workspace.naman@gmail.com>
2024-12-26 18:02:34 +0530

    added contextapi for cart functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,49 @@
-import { Button, IconButton } from "@mui/material";
-import logoImg from "../assets/logo.jpg";
+// // import { Button } from "@mui/material";
+// import logoImg from "../assets/logo.jpg";
+// import Button from "./UI/Button";
+// function Header() {
+//   return (
+//     <header id="main-header">
+//       <div id="title">
+//         <img src={logoImg} alt="Logo" />
+//         <h1>ReactMeals</h1>
+//       </div>
+//       <div>
+//         <nav>
+//           {/* <button>Cart {0}</button> */}
+//           <Button>Cart ({0})</Button>
+//           {/* <IconButton color="primary" aria-label="add to shopping cart">
+//             Cart
+//           </IconButton> */}
+//         </nav>
+//       </div>
+//     </header>
+//   );
+// }
 
-function Header() {
+// export default Header;
+import { useContext } from "react";
+
+import Button from "./UI/Button.jsx";
+import logoImg from "../assets/logo.jpg";
+import CartContext from "../store/CartContext.jsx";
+
+export default function Header() {
+  const cartCtx = useContext(CartContext);
+
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
   return (
     <header id="main-header">
       <div id="title">
-        <img src={logoImg} alt="Logo" />
-        <h1>ReactMeals</h1>
+        <img src={logoImg} alt="A restaurant" />
+        <h1>ReactFood</h1>
       </div>
-      <div>
-        <nav>
-          {/* <button>Cart {0}</button> */}
-          <Button size="small" variant="contained">
-            Cart ({0})
-          </Button>
-          {/* <IconButton color="primary" aria-label="add to shopping cart">
-            Cart
-          </IconButton> */}
-        </nav>
-      </div>
+      <nav>
+        <Button textOnly>Cart ({totalCartItems})</Button>
+      </nav>
     </header>
   );
 }
-
-export default Header;
